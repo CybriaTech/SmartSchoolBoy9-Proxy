fa556fdc657dfb76650886191d638853623b41bf
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-10-31 06:32:16 -0500

    Create index.js

---
+++ public/js/index.js
@@ -0,0 +1,32 @@
+const form = document.querySelector('form', '#search-top');
+const uvbar = document.querySelector('.uv-bar', '#top-search');
+const uvframe = document.getElementById('uv-frame');
+
+form.addEventListener('submit', async event => {
+    event.preventDefault();
+    uvframe.style.display = 'flex';
+    window.navigator.serviceWorker.register('./sw.js', {
+        scope: __uv$config.prefix
+    }).then(() => {
+        let url = uvbar.value.trim();
+        if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
+        else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
+
+        var rawurl;
+
+        if (!isUrl(url)) {
+            rawurl = 'https://www.google.com/search?q=' + url;
+        } else {
+            rawurl = (url.startsWith('https://') || url.startsWith('http://')) ? url : 'http://' + url;
+        }
+
+
+        uvframe.src = __uv$config.prefix + __uv$config.encodeUrl(url);
+        document.getElementById('#top-search').value = rawurl;
+    });
+});
+
+function isUrl(val = ''){
+    if (/^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ') return true;
+    return false;
+};
