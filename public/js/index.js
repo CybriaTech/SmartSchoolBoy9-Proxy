7f6b51a6103b03ac5a294f6c23c950f96c858883
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-11-01 13:53:24 -0500

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,15 @@
-const uvform = document.querySelector('form');
-const uvbar = document.querySelector('#top-search input.uv-bar');
-/* const uvframe = document.getElementById('uv-frame');
-const burners = document.querySelectorAll('input.uv-bar, .subtitle, #title-area'); */
+const form = document.querySelector('form');
+const input = document.querySelector('#top-search input.uv-bar');
 
-uvform.addEventListener('submit', async event => {
+form.addEventListener('submit', async event => {
     event.preventDefault();
     window.navigator.serviceWorker.register('./sw.js', {
         scope: __uv$config.prefix
     }).then(() => {
-        let url = uvbar.value.trim();
+        let url = input.value.trim();
         if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
         else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
+
 
         window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
     });
