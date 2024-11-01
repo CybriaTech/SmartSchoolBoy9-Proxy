f4adca62ca641ac5723d052bcb56a502cd771597
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-11-01 07:49:13 -0500

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,29 +1,51 @@
-import createServer from '@tomphttp/bare-server-node';
-import http from 'http';
-import nodeStatic from 'node-static';
+/**
+ * Example of serving static files and running a bare server.
+ * This is a very common setup.
+ */
+import { createServer } from "node:http";
+import { fileURLToPath } from "node:url";
+import { createBareServer } from "@tomphttp/bare-server-node";
+import serveStatic from "serve-static";
 
+const httpServer = createServer();
 
-const bare =  createServer('/bare/');
-const serve = new nodeStatic.Server('public/');
+// Run the Bare server in the /bare/ namespace. This will prevent conflicts between the static files and the bare server.
+const bareServer = createBareServer("/bare/");
 
-const server = http.createServer();
-
-server.on('request', (req, res) => {
-    if (bare.shouldRoute(req)) {
-		bare.routeRequest(req, res);
-	} else {
-		serve.serve(req, res);
-	}
+// The static root is usually relative to the main script in projects that use the Bare server.
+// ie. if static.js is at /src/static.js, public will be /public/
+// ideally, you will point the public directory relative to the current working directory
+// serveStatic('./public/')
+// This would ignore the relative location of static.js
+const serve = serveStatic(fileURLToPath(new URL("public/", import.meta.url)), {
+  fallthrough: false,
 });
 
-server.on('upgrade', (req, socket, head) => {
-	if (bare.shouldRoute(req, socket, head)) {
-		bare.routeUpgrade(req, socket, head);
-	}else{
-		socket.end();
-	}
+httpServer.on("request", (req, res) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeRequest(req, res);
+  } else {
+    serve(req, res, (err) => {
+      res.writeHead(err?.statusCode || 500, {
+        "Content-Type": "text/plain",
+      });
+      res.end(err?.stack);
+    });
+  }
 });
 
-server.listen({
-	port: process.env.PORT || 8080,
+httpServer.on("upgrade", (req, socket, head) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
+});
+
+httpServer.on("listening", () => {
+  console.log("SmartSchoolBoy9 Has Arrived");
+});
+
+httpServer.listen({
+  port: 8080,
 });
